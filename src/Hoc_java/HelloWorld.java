96ed68d200535b6789317add3ea4d0c6bdfd289f
nvHieuQG <nguyenh2409@gmail.com>
2025-06-10 16:59:42 +0700

    init project

---
+++ src/Hoc_java/HelloWorld.java
@@ -0,0 +1,11 @@
+package Hoc_java;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with java and Nguyen Van Hieu");
+		System.out.println("Hello World with java and Nguyen Van Hieu");
+		System.out.println("Hello World with java and Nguyen Van Hieu");
+		System.out.println("Hello World with java and Nguyen Van Hieu");
+
+	}
+}
